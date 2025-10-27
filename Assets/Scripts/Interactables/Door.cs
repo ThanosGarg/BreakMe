780e98b266248b9c3ed2161d63cd355094232086
ThanosGarg <thanosgargoulkas@gmail.com>
2025-10-27 13:34:28 +0100

    Add Door interactable and update scene
    Introduced a new Door interactable script and added a Door GameObject to the POC scene. Moved ItemHolder scripts to the Interactables folder for better organization.

---
+++ Assets/Scripts/Interactables/Door.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Door : Interactable 
+{
+ public override void Interact()
+    {
+      base.Interact(); 
+        Debug.Log("The door " + gameObject.name + " has been opened!");
+        // Additional door opening logic can be added here
+    }
+}
