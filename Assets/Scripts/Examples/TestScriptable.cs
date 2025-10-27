6e96bccf8701524df9363fba1f0088e668c82663
Kyriakos Triantafyllidis <kiriakostriadafillidis@gmail.com>
2025-10-27 14:47:23 +0200

    Add TestScriptable and item pickup to Player
    Introduced a new TestScriptable ScriptableObject and related asset files for testing. Added a PickUpItem method and inventory update logic to the Player class. Updated the POC scene to reference the new test item asset and made minor MeshRenderer property adjustments.

---
+++ Assets/Scripts/Examples/TestScriptable.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewTestScriptable", menuName = "TestScriptable")]
+public class TestScriptable : ScriptableObject
+{
+
+}
--- Assets/Scripts/PlayerCodeBase/Player.cs
+++ Assets/Scripts/PlayerCodeBase/Player.cs
@@ -5,4 +5,18 @@ public class Player : MonoBehaviour
 {
     public int health = 100;
     public List<Item> inventory = new List<Item>();
+
+    public void PickUpItem(Item item)
+    {
+        inventory.Add(item);
+        item.PickUp();
+    }
+
+    void Update()
+    {
+        foreach (var item in inventory)
+        {
+            item.UpdateItem();
+        }
+    }
 }
